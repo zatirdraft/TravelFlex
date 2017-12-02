325be389fb3c0fa8406e4fb3ac089b9e5fcc60a7
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-12-02 19:28:21 +0000

    Had the P2P ports backwards

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 54322 : 44322;
+    return testnet ? 44322 : 54322;
 }
 
 
