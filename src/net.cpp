08bdfbab85444a0d692f53b72f61f3b897973de2
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-12-03 05:47:45 +0000

    Add DNS seed nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1187,15 +1187,13 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-      {"US-West", "165.227.51.119"},
-      {"US-East", "45.55.142.171"},
-      {"UK", "138.68.136.38"},
-      {"CAN", "138.197.168.162"},
+    {"seed", "seed.travelflex.mycryptocoins.net"},
+    {"seednodes", "seednodes.travelflex.mycryptocoins.net"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-      {"NULL", "0.0.0.0"},
+    {"NULL", "0.0.0.0"},
     {NULL, NULL}
 };
 
